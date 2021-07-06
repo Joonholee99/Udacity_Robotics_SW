a55283efdc566944697454bb87dcf10cfd7ee5c4
Joonholee99 <Joonholee99@gmail.com>
2021-07-06 21:17:36 +0900

    7/6

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/joonho/Udacity/catkin_ws/src
+# Build directory: /home/joonho/Udacity/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(FirstPackage)
+subdirs(simple_arm)
---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/joonho/Udacity/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/joonho/Udacity/catkin_ws/devel;/opt/ros/kinetic'.split(';') if '/home/joonho/Udacity/catkin_ws/devel;/opt/ros/kinetic' != '' else []
